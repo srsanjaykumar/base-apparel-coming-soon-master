77935f423aee6d2b731849485cd4e6c34d5eed65
srsanjaykumar <136292754+srsanjaykumar@users.noreply.github.com>
2025-06-11 12:53:37 +0530

    image resize is pending

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const mail = document.querySelector("#email");
 const send_btn = document.querySelector("button");
 const email_element = document.querySelector(".main");
 const error_message = "Please provide a valid email address";
-
+const mail_box = document.querySelector(".email");
 const p = document.createElement("p");
 p.classList.add("error");
 p.textContent = error_message;
 
-const img = document.createElement('img');
-img.src = "images/icon-error.svg";
-img.classList.add("error-icon");
+const error_img = document.createElement('img');
+error_img.src = "images/icon-error.svg";
+error_img.classList.add("error-icon");
 
 
-console.log(mail, send_btn);
+
 
 function validate_email(email) {
   if (regex.test(email)) {
@@ -23,18 +23,26 @@ function validate_email(email) {
     return false;
   }
 }
+
+
+// email error message
 send_btn.addEventListener("click", (e) => {
   e.preventDefault();
   const email = mail.value;
   console.log(email);
   if (validate_email(email)) {
+    mail_box.style.border ="1px solid var(--desaturated-red)";
     p.remove();
-    img.remove();
+    error_img.remove();
     console.log("Good email");
 
   } else {
-    
-    mail.insertAdjacentElement("afterend", img);
+
+    mail_box.style.border = "2px solid hsl(0, 93%, 68%)";
+    mail.insertAdjacentElement("afterend", error_img);
     email_element.appendChild(p);
   }
 });
+
+
+
